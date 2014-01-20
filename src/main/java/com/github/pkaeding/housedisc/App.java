f7ca204d85a5f2c1cb070fce8134a187fcc4f796
Patrick Kaeding <patrick@kaeding.name>
2014-01-19 20:40:38 -0800

    new approach at border detection; identified left & right borders sucessfully

--- src/main/java/com/github/pkaeding/housedisc/App.java
+++ src/main/java/com/github/pkaeding/housedisc/App.java
@@ -11,6 +11,7 @@ import java.awt.image.BufferedImage;
 import java.awt.image.BufferedImageOp;
 import java.awt.image.ColorConvertOp;
 import java.awt.image.DataBufferByte;
+import java.awt.image.Raster;
 import java.io.File;
 import java.io.IOException;
 import java.util.Comparator;
@@ -38,22 +39,106 @@ public class App {
 	public static void main(String[] args) {
 		ImageIO.scanForPlugins();
 		IIORegistry.getDefaultInstance().registerApplicationClasspathSpis();
-		Iterator<ImageReader> ir = ImageIO.getImageReadersByFormatName("pbm");
-		while (ir.hasNext()) {
-			ImageReader r = ir.next();
-			System.out.println("can read raster: " + r.canReadRaster());
-			System.out.println(r);
-		}
 		String filename = args[0];
 		try {
 			BufferedImage inputImage = ImageIO.read(ImageIO
 					.createImageInputStream(new File(filename)));
-			processPage(inputImage);
+			processPage2(inputImage);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		System.out.println("Hello World!");
+	}
+	
+	/**
+	 * Scans inward from the left of the image, looking for a lot of black pixels.  Just how many 
+	 * is a lot is determined by the threshold provided
+	 * @param image
+	 * @param threshold between 0 and 1.0, the ratio that will be the minimum to be considered a
+	 * border.  For instance, if you want to find a column that is 80% black pixels, pass in 0.8.
+	 * @return the x coordinate that has more than the threshold black pixels, or -1 if no such 
+	 * colum was found.
+	 */
+	static int findLeftBorder(BufferedImage image, double threshold) {
+		Raster raster = image.getData();
+		int minX = raster.getMinX();
+		int minY = raster.getMinY();
+		int maxX = raster.getWidth() + minX;
+		int height = raster.getHeight();
+		boolean foundBlack = false;
+		for (int i = minX; i < maxX; i++) {
+			int[] pixels = raster.getPixels(i, minY, 1, height, new int[height]);
+			int numBlack = countBlackPixels(pixels);
+			if (((double) numBlack) / height > threshold) {
+				foundBlack = true;
+			} else if (foundBlack == true) {
+				return i;
+			}
+		}
+		return -1;
+	}
+	
+	/**
+	 * Scans inward from the right of the image, looking for a lot of black pixels.  Just how many 
+	 * is a lot is determined by the threshold provided
+	 * @param image
+	 * @param threshold between 0 and 1.0, the ratio that will be the minimum to be considered a
+	 * border.  For instance, if you want to find a column that is 80% black pixels, pass in 0.8.
+	 * @return the x coordinate that has more than the threshold black pixels, or -1 if no such 
+	 * colum was found.
+	 */
+	static int findRightBorder(BufferedImage image, double threshold) {
+		Raster raster = image.getData();
+		int minX = raster.getMinX();
+		int minY = raster.getMinY();
+		int maxX = raster.getWidth() + minX;
+		int height = raster.getHeight() - 1;
+		boolean foundBlack = false;
+		for (int i = maxX - 1; i >= minX; i--) {
+			int[] pixels = raster.getPixels(i, minY, 1, height, new int[height]);
+			int numBlack = countBlackPixels(pixels);
+			if (((double) numBlack) / height > threshold) {
+				foundBlack = true;
+			} else if (foundBlack == true) {
+				return i;
+			}
+		}
+		return -1;
+	}
+	
+	static int countBlackPixels(int[] pixels) {
+		int ret = 0;
+		for (int i = 0; i < pixels.length; i++) {
+			if ((pixels[i] & 0x00FFFFFF) == 0) {
+				ret++;
+			}
+		}
+		return ret;
+	}
+	
+	static void processPage2(BufferedImage page) throws IOException {
+		String id = UUID.randomUUID().toString();
+		BufferedImage rotated = rotateLeft90degrees(page);
+		ImageIO.write(rotated, "png", new File("/tmp/" + id + "-rotated.png"));
+		BufferedImage img = toGrayscale(rotated);
+		ImageIO.write(img, "png", new File("/tmp/" + id + "-gray.png"));
+		int leftBorder = findLeftBorder(img, 0.6);
+		int rightBorder = findRightBorder(img, 0.6);
+		Graphics2D graphics = rotated.createGraphics();
+		graphics.setColor(Color.blue);
+		int height = rotated.getHeight() - 1;
+		
+		if (leftBorder > 0)
+			graphics.draw(new Line2D.Double(leftBorder, 0, leftBorder, height));
+		else 
+			System.out.println("no left border found");
+		
+		if (rightBorder > 0)
+			graphics.draw(new Line2D.Double(rightBorder, 0, rightBorder, height));
+		else 
+			System.out.println("no right border found");
+		
+		ImageIO.write(rotated, "png", new File("/tmp/" + id + "-lines.png"));
 	}
 
 	static void processPage(BufferedImage page) throws IOException {
@@ -81,13 +166,13 @@ public class App {
 		System.out.println("lines: " + lines.dump());
 		System.out.println("found " + lines.rows() + " lines");
 		System.out.println("found " + lines.cols() + " cols");
-		for (int i = 0; i < lines.cols(); i++) 
+		for (int i = 0; i < lines.cols() && i < 5; i++) 
 		{
 			double[] vec = lines.get(0, i);
-			double y2 = vec[0];
-			double x2 = vec[1];
-			double y1 = vec[2];
-			double x1 = vec[3];
+			double y1 = vec[0];
+			double x1 = vec[1];
+			double y2 = vec[2];
+			double x2 = vec[3];
 	          
 //			if (Math.abs(y1 - y2) < 20) {
 				System.out.println(String.format("p1: (%s, %s) p2: (%s, %s)",
